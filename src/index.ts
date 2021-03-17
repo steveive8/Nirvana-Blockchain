f92239d3d5af09cec8b6779f8c811b335dfe192c
johnsteve108 <buddhychoi@gmail.com>
2021-03-17 11:53:09 +0900

    index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello")
+/* 
+
+Nirvan Blockchain 
+
+MIT License
+Copyright (c) 2021 Leonardo Steve
+
+The whole new concept of Blockchain that suggest the better approach of how 
+the service and web can communicate with the users, and how can be capability with
+the web3.js of Ethereum.
+
+*/
+
+class Block {
+
+}
+
+class Transaction {
+    
+}
